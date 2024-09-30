63bae4a55263cd3ceb249cba28f1c648d9325536
Vontainment <djav1985@gmail.com>
2024-09-30 07:56:09 -0400

    Update docker-entrypoint.sh

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
-if [ $UID -eq 0 ]; then
+# Check if running as root to modify ulimit
+if [ "$UID" -eq 0 ]; then
   ulimit -n 65535
   echo "NEW ULIMIT: $(ulimit -n)"
 else
-  echo ENTRYPOINT DID NOT RUN AS ROOT
+  echo "ENTRYPOINT DID NOT RUN AS ROOT"
 fi
 
-if [ $FLY_PROCESS_GROUP = "app" ]; then
+# Check the FLY_PROCESS_GROUP environment variable
+if [ "$FLY_PROCESS_GROUP" = "app" ]; then
   echo "RUNNING app"
   node --max-old-space-size=8192 dist/src/index.js
-elif [ $FLY_PROCESS_GROUP = "worker" ]; then
+elif [ "$FLY_PROCESS_GROUP" = "worker" ]; then
   echo "RUNNING worker"
   node --max-old-space-size=8192 dist/src/services/queue-worker.js
 else
